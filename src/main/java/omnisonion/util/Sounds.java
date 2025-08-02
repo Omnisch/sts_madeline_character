727d0cda8fc2e27e77f8545bb18f8a2b089842e2
Omnisch <fleurdeln@gmail.com>
2025-08-02 16:34:24 +0800

    init commit

---
+++ src/main/java/omnisonion/util/Sounds.java
@@ -0,0 +1,8 @@
+package omnisonion.util;
+
+import static omnisonion.OnionMod.audioPath;
+
+public class Sounds {
+    public static String TEST_SOUND = audioPath("test.wav"); //Load audio using a given path
+    public static String ding; //Load audio from audioPath based on the field name
+}
