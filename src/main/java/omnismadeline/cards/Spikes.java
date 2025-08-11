06425a341421aa55757fd535927d17a139cd45d5
Omnisch <fleurdeln@gmail.com>
2025-08-11 23:28:37 +0800

    add Spikes

---
+++ src/main/java/omnismadeline/cards/Spikes.java
@@ -0,0 +1,47 @@
+package omnismadeline.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.ThornsPower;
+import omnismadeline.character.MadelineCharacter;
+import omnismadeline.stances.StayStance;
+import omnismadeline.util.CardStats;
+
+public class Spikes extends BaseEnvironmentCard {
+    public static final String ID = makeID(Spikes.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MadelineCharacter.Meta.CARD_COLOR,
+            CardType.ATTACK, // ATTACK / SKILL / POWER / CURSE / STATUS
+            CardRarity.COMMON, // BASIC / COMMON / UNCOMMON / RARE / SPECIAL / CURSE
+            CardTarget.ENEMY,
+            -2
+    );
+
+    private static final int DAMAGE = 8;
+    private static final int UPG_DAMAGE = 2;
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 1;
+
+    public Spikes() {
+        super(ID, info);
+        setDamage(DAMAGE, UPG_DAMAGE);
+        setMagic(MAGIC, UPG_MAGIC);
+    }
+
+    @Override
+    protected void onUse(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL)));
+        this.addToBot(new ApplyPowerAction(p, p, new ThornsPower(p, magicNumber), magicNumber));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Spikes();
+    }
+}
