b644104b8c77e7bc68ad6f4fd3978f878b25b0f4
Omnisch <fleurdeln@gmail.com>
2025-08-18 20:20:51 +0800

    add Wind

---
+++ src/main/java/omnismadeline/cards/Wind.java
@@ -0,0 +1,44 @@
+package omnismadeline.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import omnismadeline.character.MadelineCharacter;
+import omnismadeline.util.CardStats;
+
+public class Wind extends BaseCard {
+    public static final String ID = makeID(Wind.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MadelineCharacter.Meta.CARD_COLOR,
+            CardType.SKILL, // ATTACK / SKILL / POWER / CURSE / STATUS
+            CardRarity.UNCOMMON, // BASIC / COMMON / UNCOMMON / RARE / SPECIAL / CURSE
+            CardTarget.NONE,
+            0
+    );
+
+    private static final int VULNERABLE = 3;
+    private static final int WEAK = 2;
+
+    public Wind() {
+        super(ID, info);
+        setSelfRetain(false, true);
+        setExhaust(true);
+    }
+
+    @Override
+    protected void onUse(AbstractPlayer p, AbstractMonster m) {
+        for (AbstractMonster em : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            this.addToBot(new ApplyPowerAction(em, p, new VulnerablePower(em, VULNERABLE, false), VULNERABLE));
+        }
+        this.addToBot(new ApplyPowerAction(p, p, new WeakPower(p, WEAK, false), WEAK));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Wind();
+    }
+}
