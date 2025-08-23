4d3e8881fa909e8652e94ded7d904c427e2920f6
Omnisch <fleurdeln@gmail.com>
2025-08-23 13:57:34 +0800

    add card RedBubble

---
+++ src/main/java/omnismadeline/cards/RedBubble.java
@@ -0,0 +1,46 @@
+package omnismadeline.cards;
+
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import omnismadeline.actions.MadelineRefillAction;
+import omnismadeline.character.MadelineCharacter;
+import omnismadeline.util.CardStats;
+
+public class RedBubble extends BaseCard {
+    public static final String ID = makeID(RedBubble.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MadelineCharacter.Meta.CARD_COLOR,
+            CardType.ATTACK, // ATTACK / SKILL / POWER / CURSE / STATUS
+            CardRarity.UNCOMMON, // BASIC / COMMON / UNCOMMON / RARE / SPECIAL / CURSE
+            CardTarget.ENEMY,
+            1
+    );
+
+    private static final int DAMAGE = 6;
+    private static final int MAGIC = 2;
+
+    public RedBubble() {
+        super(ID, info);
+        setDamage(DAMAGE);
+        setMagic(MAGIC);
+        setCostUpgrade(0);
+    }
+
+    @Override
+    protected void onUse(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL)));
+        this.addToBot(new DiscardAction(p, p, 1, false));
+        this.addToBot(new DrawCardAction(2));
+        this.addToBot(new MadelineRefillAction());
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new RedBubble();
+    }
+}
