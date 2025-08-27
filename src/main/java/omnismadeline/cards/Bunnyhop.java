803994d6ba7c58bd4ff388c19af336cea8e37eed
Omnisch <fleurdeln@gmail.com>
2025-08-27 23:36:30 +0800

    add Bunnyhop

---
+++ src/main/java/omnismadeline/cards/Bunnyhop.java
@@ -0,0 +1,46 @@
+package omnismadeline.cards;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import omnismadeline.actions.MadelineGainMomentumAction;
+import omnismadeline.actions.MadelineMoveAction;
+import omnismadeline.character.MadelineCharacter;
+import omnismadeline.enums.CustomTags;
+import omnismadeline.util.CardStats;
+
+public class Bunnyhop extends BaseJumpCard {
+    public static final String ID = makeID(Bunnyhop.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MadelineCharacter.Meta.CARD_COLOR,
+            CardType.SKILL, // ATTACK / SKILL / POWER / CURSE / STATUS
+            CardRarity.COMMON, // BASIC / COMMON / UNCOMMON / RARE / SPECIAL / CURSE
+            CardTarget.ENEMY,
+            1
+    );
+
+    private static final int BLOCK = 5;
+    private static final int UPG_BLOCK = 3;
+    private static final int MAGIC = 1;
+    private static final int UPG_MAGIC = 1;
+
+    public Bunnyhop() {
+        super(ID, info);
+        this.setBlock(BLOCK, UPG_BLOCK);
+        this.setMagic(MAGIC, UPG_MAGIC);
+        this.tags.add(CustomTags.MOVE);
+    }
+
+    @Override
+    protected void onUse(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, p, this.block));
+        this.addToBot(new MadelineGainMomentumAction(this.magicNumber));
+        this.addToBot(new MadelineMoveAction(m, GAP));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Bunnyhop();
+    }
+}
