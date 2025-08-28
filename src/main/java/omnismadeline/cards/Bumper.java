3be2d2473f7a35aad1b0a4ba697ce9b34bd17d0a
Omnisch <fleurdeln@gmail.com>
2025-08-28 14:56:06 +0800

    add Bumper

---
+++ src/main/java/omnismadeline/cards/Bumper.java
@@ -0,0 +1,45 @@
+package omnismadeline.cards;
+
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import omnismadeline.actions.MadelineRefillAction;
+import omnismadeline.character.MadelineCharacter;
+import omnismadeline.util.CardStats;
+
+public class Bumper extends BaseEnvironmentCard {
+    public static final String ID = makeID(Bumper.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MadelineCharacter.Meta.CARD_COLOR,
+            CardType.ATTACK, // ATTACK / SKILL / POWER / CURSE / STATUS
+            CardRarity.UNCOMMON, // BASIC / COMMON / UNCOMMON / RARE / SPECIAL / CURSE
+            CardTarget.ENEMY,
+            0
+    );
+
+    private static final int DAMAGE = 4;
+    private static final int UPG_DAMAGE = 2;
+    private static final int BLOCK = 4;
+    private static final int UPG_BLOCK = 2;
+
+    public Bumper() {
+        super(ID, info);
+        this.setDamage(DAMAGE, UPG_DAMAGE);
+        this.setBlock(BLOCK, UPG_BLOCK);
+    }
+
+    @Override
+    protected void onUse(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, this.block));
+        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL)));
+        this.addToBot(new MadelineRefillAction());
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Bumper();
+    }
+}
