318f946e6abb2a1f2710a29cbd6fe81fa2d9f621
Omnisch <fleurdeln@gmail.com>
2025-08-29 20:29:15 +0800

    add card WaveDash

--- src/main/java/omnismadeline/cards/WaveDash.java
+++ src/main/java/omnismadeline/cards/WaveDash.java
@@ -2,16 +2,17 @@ package omnismadeline.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import omnismadeline.actions.MadelineGainMomentumAction;
 import omnismadeline.character.MadelineCharacter;
+import omnismadeline.enums.CustomTags;
 import omnismadeline.util.CardStats;
 
-public class Kevin extends BaseDashCard {
-    public static final String ID = makeID(Kevin.class.getSimpleName());
+public class WaveDash extends BaseDashCard {
+    public static final String ID = makeID(WaveDash.class.getSimpleName());
     private static final CardStats info = new CardStats(
             MadelineCharacter.Meta.CARD_COLOR,
             CardType.ATTACK, // ATTACK / SKILL / POWER / CURSE / STATUS
@@ -20,25 +21,26 @@ public class Kevin extends BaseDashCard {
             2
     );
 
-    private static final int DAMAGE = 15;
-    private static final int UPG_DAMAGE = 4;
-    private static final int MAGIC = 1;
-    private static final int UPG_MAGIC = 1;
+    private static final int DAMAGE = 14;
+    private static final int UPG_DAMAGE = 3;
+    private static final int MAGIC = 2;
 
-    public Kevin() {
+    public WaveDash() {
         super(ID, info);
         this.setDamage(DAMAGE, UPG_DAMAGE);
-        this.setMagic(MAGIC, UPG_MAGIC);
+        this.setMagic(MAGIC);
     }
 
     @Override
     protected void onUse(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SMASH));
-        this.addToBot(new MadelineGainMomentumAction(this.magicNumber));
+        if (this.movedFromCardTag == CustomTags.JUMP) {
+            this.addToBot(new GainEnergyAction(this.magicNumber));
+        }
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new Kevin();
+        return new WaveDash();
     }
 }
