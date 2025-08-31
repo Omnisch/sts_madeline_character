86a08034d8fa6d259899883bd7001ebb160d46fb
Omnisch <fleurdeln@gmail.com>
2025-08-31 22:22:03 +0800

    fix DashChancePower description

--- src/main/java/omnismadeline/powers/DashChancePower.java
+++ src/main/java/omnismadeline/powers/DashChancePower.java
@@ -16,6 +16,7 @@ public class DashChancePower extends BasePower {
 
     public DashChancePower(AbstractCreature owner, int amount) {
         super(POWER_ID, TYPE, TURN_BASED, owner, amount);
+        this.updateDescription();
     }
 
     public void updateDescription() {
