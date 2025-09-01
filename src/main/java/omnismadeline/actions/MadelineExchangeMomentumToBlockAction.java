5b06b25298d72615d87747dd9e2f3cb76234be4e
Omnisch <fleurdeln@gmail.com>
2025-09-01 13:22:13 +0800

    add card BadelineBubble

---
+++ src/main/java/omnismadeline/actions/MadelineExchangeMomentumToBlockAction.java
@@ -0,0 +1,40 @@
+package omnismadeline.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import omnismadeline.powers.DashChancePower;
+import omnismadeline.powers.MomentumPower;
+
+public class MadelineExchangeMomentumToBlockAction extends AbstractGameAction {
+    private final float scale;
+
+    public MadelineExchangeMomentumToBlockAction(float scale) {
+        this.duration = this.startDuration = Settings.ACTION_DUR_FAST;
+        this.scale = scale;
+    }
+
+    @Override
+    public void update() {
+        AbstractPlayer p = AbstractDungeon.player;
+
+        int momentumAmount = 0;
+        if (p.hasPower(MomentumPower.POWER_ID)) {
+            momentumAmount += p.getPower(MomentumPower.POWER_ID).amount;
+        }
+
+        if (momentumAmount == 0) {
+            this.isDone = true;
+            return;
+        }
+
+        this.addToBot(new RemoveSpecificPowerAction(p, p, MomentumPower.POWER_ID));
+        this.addToBot(new GainBlockAction(p, (int) (this.scale * momentumAmount)));
+
+        this.isDone = true;
+    }
+}
---
+++ src/main/java/omnismadeline/cards/BadelineBubble.java
@@ -0,0 +1,40 @@
+package omnismadeline.cards;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import omnismadeline.actions.MadelineExchangeMomentumToBlockAction;
+import omnismadeline.actions.MadelineRefillAction;
+import omnismadeline.character.MadelineCharacter;
+import omnismadeline.util.CardStats;
+
+public class BadelineBubble extends BaseCard {
+    public static final String ID = makeID(BadelineBubble.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MadelineCharacter.Meta.CARD_COLOR,
+            CardType.SKILL, // ATTACK / SKILL / POWER / CURSE / STATUS
+            CardRarity.RARE, // BASIC / COMMON / UNCOMMON / RARE / SPECIAL / CURSE
+            CardTarget.SELF,
+            0
+    );
+
+    private static final int MAGIC = 4;
+    private static final int UPG_MAGIC = 1;
+
+    public BadelineBubble() {
+        super(ID, info);
+        this.setMagic(MAGIC, UPG_MAGIC);
+        this.setExhaust(true);
+    }
+
+    @Override
+    protected void onUse(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new MadelineExchangeMomentumToBlockAction(this.magicNumber));
+        this.addToBot(new MadelineRefillAction());
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new BadelineBubble();
+    }
+}
