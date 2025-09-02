5ee8b9b137e461e657c23c047beeca4967b7bb25
Omnisch <fleurdeln@gmail.com>
2025-09-02 10:39:32 +0800

    add card Wallbounce

---
+++ src/main/java/omnismadeline/cards/jumps/Wallbounce.java
@@ -0,0 +1,47 @@
+package omnismadeline.cards.jumps;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import omnismadeline.character.MadelineCharacter;
+import omnismadeline.enums.CustomTags;
+import omnismadeline.util.CardStats;
+
+public class Wallbounce extends BaseJumpCard {
+    public static final String ID = makeID(Wallbounce.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MadelineCharacter.Meta.CARD_COLOR,
+            CardType.ATTACK, // ATTACK / SKILL / POWER / CURSE / STATUS
+            CardRarity.UNCOMMON, // BASIC / COMMON / UNCOMMON / RARE / SPECIAL / CURSE
+            CardTarget.ENEMY,
+            1
+    );
+
+    private static final int DAMAGE = 8;
+    private static final int UPG_DAMAGE = 2;
+    private static final int BLOCK = 5;
+    private static final int UPG_BLOCK = 3;
+
+    public Wallbounce() {
+        super(ID, info);
+        this.setBlock(BLOCK, UPG_BLOCK);
+        this.setDamage(DAMAGE, UPG_DAMAGE);
+    }
+
+    @Override
+    protected void onUse(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        if (this.movedFromCardTag == CustomTags.DASH) {
+            this.addToBot(new GainBlockAction(p, p, this.block));
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Wallbounce();
+    }
+}
